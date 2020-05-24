508985c66e8eb00a7d8757bb9697f2f7ce472e4d
szymanskir <ryszard.szymanski@outlook.com>
2020-05-24 18:15:30 +0200

    Add initial analysis with basic metrics (#7)

--- make.R
+++ make.R
@@ -16,7 +16,33 @@ plan <- drake::drake_plan(
   depends_graph = graph_from_data_frame(
     d = direct_graph_df$edges, 
     vertices = direct_graph_df$vertices
-  )
+  ),
+  
+  degree_hist_plot = target(
+    degree_hist(graph, mode = "in", title = title),
+    transform = map(
+      graph = list(suggests_graph, depends_graph),
+      title = c("Suggests graph", "Depends/Imports graph"),
+      .id = graph
+    ),
+  ),
+  summary = target(
+    summarise_graph(graph, rowname),
+    transform = map(
+      graph = list(suggests_graph, depends_graph),
+      rowname = c("Suggests", "Deps")
+    )
+  ),
+  graphs_summary = target(
+    data.table::rbindlist(list(summary)),
+    transform = combine(summary)
+  ),
+  
+  top = target(
+    top_n_pkgs(graph),
+    transform = map(graph = list(suggests_graph, depends_graph))
+  ),
+  initial_analysis_report = rmarkdown::render(knitr_in("notebooks/01-initial_analysis.Rmd"))
 )
 
 drake::make(plan)
